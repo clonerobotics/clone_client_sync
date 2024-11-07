2ebd953f2f4f363c65658c156bdd037a127d9c78
Maciej Bakowicz <maciej.bakowicz@clonerobotics.com>
2024-11-06 23:57:30 -0800

    Update to version 0.0.8

--- clone_client_sync/utils.py
+++ clone_client_sync/utils.py
@@ -1,5 +1,46 @@
 from contextlib import contextmanager
-from time import perf_counter, sleep
+from time import perf_counter, perf_counter_ns, sleep
+from typing import Generator
+import warnings
+
+import ctypes
+try:
+    libc = ctypes.CDLL('libc.so.6')
+    nanosleep = libc.nanosleep
+except OSError:
+    warnings.warn("Failed to import libc. Using python sleep instead.", ImportWarning)
+
+    def nanosleep(p: int) -> None:
+        """Wrapper for nanosleep function if C library import fails."""
+        sleep(p / 1e9)
+
+
+def precise_interval(interval: float, tolerance: float = 1e-4) -> Generator[None, None, None]:
+    """
+    Interval ticks for precise timeings.
+
+    Parameters:
+    - interval: Duration between each tick in seconds.
+    - tolerance: Acceptable drift in seconds for each tick.
+    """
+    interval_ns = int(interval * 1e9)
+    tolerance_ns = int(tolerance * 1e9)
+    next_tick = perf_counter_ns() + interval_ns
+
+    try:
+        while True:
+            remaining = next_tick - perf_counter_ns()
+
+            if remaining > tolerance_ns:
+                nanosleep(remaining - tolerance_ns)
+            elif remaining > 0:
+                nanosleep(remaining)
+
+            yield
+
+            next_tick += interval_ns
+    except GeneratorExit:
+        pass
 
 
 @contextmanager
@@ -9,7 +50,7 @@ def busy_ticker(dur: float, precision: float = 5, min_tick: float = 0.0005):
     ensuring precise timing of the loop.
     See original implementation in clone_client/utils.py.
     """
-
+    warnings.warn("This method is deprecated and will be removed in future versions. Use 'precise_interval' instead", DeprecationWarning)
     next_tick = perf_counter() + dur
 
     yield
@@ -21,5 +62,3 @@ def busy_ticker(dur: float, precision: float = 5, min_tick: float = 0.0005):
     # Busy sleep until next tick for precise timing
     while perf_counter() < next_tick:
         sleep(min_tick)
-
-
